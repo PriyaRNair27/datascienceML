534799045e68c4e9f3aeaddaed16ca4c0ec5b630
PriyaRNair27 <80029151+PriyaRNair27@users.noreply.github.com>
2022-02-24 11:27:21 +0530

    Add files via upload

--- can.py
+++ can.py
@@ -7,6 +7,8 @@ x_train,x_test,y_train,y_test=train_test_split(cancer.data,cancer.target,test_si
 clf=svm.SVC(kernel='linear')
 clf.fit(x_train,y_train)
 y_pred=clf.predict(x_test)
+print("Actual values:",y_test)
+print("Predicted values:",y_pred)
 print("Accuracy:",metrics.accuracy_score(y_test,y_pred))
 print("Precision:",metrics.precision_score(y_test,y_pred))
 print("Recall:",metrics.recall_score(y_test,y_pred))
